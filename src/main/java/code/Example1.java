40f83e34f93354288a3a0b9cd8a2a29d64358170
Ankit Ojha <ojhaankit@outlook.com>
2017-10-04 23:42:58 +0530

    Code Style Change

--- src/main/java/code/Example1.java
+++ src/main/java/code/Example1.java
@@ -2,9 +2,9 @@ package code;
 
 public class Example1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-     System.out.println("Hellow World");
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello World");
+    }
 
 }
